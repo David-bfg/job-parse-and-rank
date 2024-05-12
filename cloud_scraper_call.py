ccc71f517781324ff1e75b4a9178951b4a4d2f46
david-bfg <david.1.bfg@gmail.com>
2024-05-11 22:10:11 -0500

    add file to directly call cloudscrapper

---
+++ cloud_scraper_call.py
@@ -0,0 +1,17 @@
+import cloudscraper
+
+def cloud_scrape():
+    scraper = cloudscraper.create_scraper(
+        browser={
+            'browser': 'chrome',
+            'platform': 'linux'
+        }
+    )
+    args = sys.argv[1:]
+
+    print(scraper.get(args[0]).text)
+       
+
+if __name__ == "__main__":
+    cloud_scrape()
+    
